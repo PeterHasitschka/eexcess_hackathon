6016a4eb16bf0095ce9a931bc17111bb5ef6867f
Peter Hasitschka <hasitschka@student.tugraz.at>
2015-10-08 11:30:37 +0200

    Removed WebGlVisualization for Submodule usage

--- visualizations/WebGlVisualization/html/standalone/standalone.js
+++
@@ -1,8 +0,0 @@
-var canvas1, textElm;
-
-jQuery(document).ready(function () {
-    GLVIS.InitHandler.init(jQuery('body'), "../../", function () {
-
-        GLVIS.Debugger.debug("standalone", "Finished initializing scene. Callback", 3);
-    });
-});
